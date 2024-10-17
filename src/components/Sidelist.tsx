bbeadb137d27ddd11648512818395eaad196d763
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-17 15:29:10 +0100

    - fixing messages list scroll bug

--- src/components/Sidelist.tsx
+++ src/components/Sidelist.tsx
@@ -9,96 +9,100 @@ import {
 } from "@/components/ui/select";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { messageList } from "@/data";
+import { Badge } from "./ui/badge";
 
 const Sidelist = () => {
 	return (
 		<section className="lg:min-w-[20%] bg-carbon ">
-			<div>
-				<Tabs defaultValue="messages" className="w-full">
-					<TabsList className="w-full h-[55px] justify-center px-4 gap-4">
-						<TabsTrigger value="messages" className="px-5 py-2 w-1/2">
-							Messages
-						</TabsTrigger>
-						<TabsTrigger value="friends" className="px-5 py-2 w-1/2">
-							Friends
-						</TabsTrigger>
-					</TabsList>
-					<div className="flex items-center justify-between gap-2 p-3">
-						<Select>
-							<SelectTrigger className="h-[40px] rounded-full bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F] text-[#FFFFFF99] flex-row-reverse justify-end font-semibold">
-								<SelectValue placeholder="Newest" />
-							</SelectTrigger>
-							<SelectContent>
-								<SelectItem value="oldest">Oldest</SelectItem>
-								<SelectItem value="read">Read</SelectItem>
-								<SelectItem value="unread">Unread</SelectItem>
-							</SelectContent>
-						</Select>
+			<Tabs defaultValue="messages" className="w-full">
+				<TabsList className="w-full h-[55px] justify-center px-4 gap-4">
+					<TabsTrigger value="messages" className="px-5 py-2 w-1/2">
+						Messages
+					</TabsTrigger>
+					<TabsTrigger value="friends" className="px-5 py-2 w-1/2">
+						Friends
+					</TabsTrigger>
+				</TabsList>
+				<div className="flex items-center justify-between gap-2 p-3">
+					<Select>
+						<SelectTrigger className="h-[40px] rounded-full bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F] text-[#FFFFFF99] flex-row-reverse justify-end font-semibold">
+							<SelectValue placeholder="Newest" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value="oldest">Oldest</SelectItem>
+							<SelectItem value="read">Read</SelectItem>
+							<SelectItem value="unread">Unread</SelectItem>
+						</SelectContent>
+					</Select>
 
-						<div className="flex gap-2 items-center ">
-							<Button className="rounded-full w-[40px] h-[40px] p-0 bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F]">
-								<img
-									src="/src/assets/icons/search.svg"
-									alt="Search"
-									width={20}
-									height={20}
-								/>
-							</Button>
+					<div className="flex gap-2 items-center ">
+						<Button className="rounded-full w-[40px] h-[40px] p-0 bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F]">
+							<img
+								src="/src/assets/icons/search.svg"
+								alt="Search"
+								width={20}
+								height={20}
+							/>
+						</Button>
 
-							<Button className="rounded-full w-[40px] h-[40px] p-0 bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F]">
-								<img
-									src="/src/assets/icons/plus.svg"
-									alt="Add"
-									width={18}
-									height={20}
-								/>
-							</Button>
-						</div>
+						<Button className="rounded-full w-[40px] h-[40px] p-0 bg-[#FFFFFF08] border border-solid border-[#FFFFFF0F]">
+							<img
+								src="/src/assets/icons/plus.svg"
+								alt="Add"
+								width={18}
+								height={20}
+							/>
+						</Button>
 					</div>
+				</div>
 
-					<TabsContent
-						value="messages"
-						className="px-3 text-[#FFFFFF99] grid gap-3"
-					>
-						{messageList.map((message, i) => {
-							const { profileImg, user, online, hasMessage } = message;
-							return (
-								<div className="flex items-center justify-start gap-3">
-									<div className="relative">
-										<Avatar className="flex items-center justify-center">
-											<AvatarImage
-												src={profileImg}
-												className="w-[35px] h-[35px]"
+				<TabsContent value="messages" className="text-[#FFFFFF99] grid">
+					{messageList.map((message, i) => {
+						const { profileImg, user, online, hasMessage, messageCount } =
+							message;
+						return (
+							<Button
+								key={i}
+								className="flex items-center justify-start h-[55px] gap-3 bg-transparent shadow-none"
+							>
+								<div className="relative">
+									<Avatar className="flex items-center justify-center">
+										<AvatarImage
+											src={profileImg}
+											className="w-[35px] h-[35px]  rounded-full"
+										/>
+										<AvatarFallback className="flex items-center justify-center">
+											<img
+												src="src/assets/icons/discord.svg"
+												className="w-[35px] h-[35px]  rounded-full"
 											/>
-											<AvatarFallback className="flex items-center justify-center">
-												<img
-													src="src/assets/icons/discord.svg"
-													className="w-[35px] h-[35px]"
-												/>
-											</AvatarFallback>
-										</Avatar>
-										<div
-											className={`absolute -right-1 bottom-0 ${
-												online ? "bg-emerald" : "bg-gray"
-											} rounded-full w-4 h-4 border-[3px] border-solid border-onyx`}
-										></div>
-									</div>
-									<span
-										className={`${
-											hasMessage ? "font-bold" : "font-normal"
-										}`}
-									>
-										{user}
-									</span>
+										</AvatarFallback>
+									</Avatar>
+									<div
+										className={`absolute -right-1 bottom-0 ${
+											online ? "bg-emerald" : "bg-gray"
+										} rounded-full w-4 h-4 border-[3px] border-solid border-onyx`}
+									></div>
 								</div>
-							);
-						})}
-					</TabsContent>
-					<TabsContent value="friends" className="px-3 text-[#FFFFFF99] ">
-						Change your friends here.
-					</TabsContent>
-				</Tabs>
-			</div>
+								<span
+									className={`${
+										hasMessage ? "font-bold" : "font-normal"
+									}`}
+								>
+									{user}
+								</span>
+
+								<Badge className="ms-auto me-0 rounded-full bg-white/10">
+									{messageCount}
+								</Badge>
+							</Button>
+						);
+					})}
+				</TabsContent>
+				<TabsContent value="friends" className="px-3 text-[#FFFFFF99] ">
+					Change your friends here.
+				</TabsContent>
+			</Tabs>
 		</section>
 	);
 };
--- src/data/index.ts
+++ src/data/index.ts
@@ -46,8 +46,20 @@ export const serverList = [
 ];
 
 export const messageList = [
-	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true},
-	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true},
-	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false},
-	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false},
+	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
+	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
+	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
+	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
+	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
+	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
+	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
+	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
+	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
+	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
+	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
+	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
+	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
+	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
+	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
+	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
 ]
