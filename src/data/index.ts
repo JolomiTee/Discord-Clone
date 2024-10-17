ae881a380250ac35bfa58bcb64af759a5db03997
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-17 15:48:23 +0100

    modified:   src/data/index.ts

--- src/data/index.ts
+++ src/data/index.ts
@@ -46,20 +46,11 @@ export const serverList = [
 ];
 
 export const messageList = [
+	{profileImg: "/beluga.png", user: "Ghost", online: true, hasMessage: true, messageCount: 2},
 	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
 	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
+	{profileImg: "/juxtopposed.png", user: "Soap", online: true, hasMessage: false, messageCount: 0},
 	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
 	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
-	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
-	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
-	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
-	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
-	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
-	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
-	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
-	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
-	{profileImg: "/beluga.png", user: "Beluga", online: true, hasMessage: true, messageCount: 2},
-	{profileImg: "/silly.png", user: "Joloo", online: true, hasMessage: true, messageCount: 5},
-	{profileImg: "/juxtopposed.png", user: "Juxtopposed", online: true, hasMessage: false, messageCount: 0},
-	{profileImg: "/jojo.png", user: "Jojo", online: false, hasMessage: false, messageCount: 0},
+	{profileImg: "/silly.png", user: "Shepard", online: true, hasMessage: true, messageCount: 5},
 ]
