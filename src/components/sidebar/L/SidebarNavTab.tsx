6fb0f4a3b177e8209e5f291d2e7b64fd10ff3d52
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-27 23:04:53 +0100

    - broke sidebar into reusable parts

---
+++ src/components/sidebar/L/SidebarNavTab.tsx
@@ -0,0 +1,53 @@
+import { NavLink } from "react-router-dom";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar";
+import { useCollapsibleIconStore } from "@/hooks/base-context";
+
+interface SidebarNavLinkProps {
+	to: string;
+	icon: string;
+	label: string;
+}
+
+export default function SidebarNavLink({
+	to,
+	icon,
+	label,
+}: SidebarNavLinkProps) {
+	const selected_tab = useCollapsibleIconStore((state) => state.selectedTab);
+	const toggle_selected_tab = useCollapsibleIconStore(
+		(state) => state.toggle_selected_tab
+	);
+	return (
+		<SidebarMenuItem className="group relative group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center">
+			<div
+				className={`absolute top-1/2 -translate-y-1/2 -left-[1px] w-2 rounded-full transition-all duration-100 ${
+					selected_tab === label.toLocaleLowerCase()
+						? "h-10 bg-discord-blue"
+						: "h-0 group-hover:h-5 bg-white opacity-0 group-hover:opacity-100"
+				}`}
+				style={{
+					clipPath: "inset(0 0 0 50%)",
+				}}
+			/>
+			<NavLink
+				to={to}
+				onClick={() => toggle_selected_tab(label.toLowerCase())}
+			>
+				<SidebarMenuButton
+					tooltip={label}
+					className="w-full gap-3 group-data-[collapsible=icon]:ps-0 text-base h-fit group-data-[collapsible=icon]:[&>span:last-child]:hidden p-0 ps-3"
+				>
+					<Avatar className="size-[50px] flex justify-center items-center">
+						<AvatarImage
+							src={`/icons/${icon}.svg`}
+							alt={`${label} icon`}
+							className="w-[30px]"
+						/>
+					</Avatar>
+					<span>{label}</span>
+				</SidebarMenuButton>
+			</NavLink>
+		</SidebarMenuItem>
+	);
+}
