8d2888e346948386a967784e632aa6f42a281f94
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-29 11:35:09 +0100

    - linking routes of inbox, servercards and help pages

--- src/components/server/ServerCard.tsx
+++ src/components/server/ServerCard.tsx
@@ -1,8 +1,14 @@
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { formatNumber } from "@/lib/utils";
+import { Link } from "react-router-dom";
+import {
+	useCollapsibleSidebarStore,
+	useSidebarStateStore,
+} from "@/hooks/base-context";
 interface DiscordServerProps {
 	name: string;
+	id: number;
 	online: number;
 	members: number;
 	lastSeen: string;
@@ -12,41 +18,58 @@ interface DiscordServerProps {
 }
 const ServerCard = ({
 	name,
+	id,
 	online,
 	members,
 	lastSeen,
 	server_img,
 }: DiscordServerProps) => {
+	const toggle_selected_server = useCollapsibleSidebarStore(
+		(state) => state.toggle_selected_server
+	);
+	const toggle_selected_tab = useCollapsibleSidebarStore(
+		(state) => state.toggle_selected_tab
+	);
+	const switchLeftSidebarContext = useSidebarStateStore(
+		(state) => state.switchLeftSidebarContext
+	);
+	const onclick = () => {
+		toggle_selected_tab(null);
+		toggle_selected_server(id);
+		switchLeftSidebarContext("server");
+	};
 	return (
-		<Card className="flex items-start gap-4 bg-transparent border-0 shadow-none">
-			<Avatar className="rounded-[8px] size-12">
-				<AvatarImage
-					src={server_img}
-					className="mx-auto my-auto object-cover"
-				/>
-				<AvatarFallback className="bg-discord-blue rounded-[8px]">
-					<img
-						src="/icons/discord.svg"
-						className="w-[35px] h-[35px] mx-auto my-auto"
+		<Link to={`/@server/${String(id)}`} onClick={onclick}>
+			<Card className="flex items-start gap-4 bg-transparent border-0 shadow-none">
+				<Avatar className="rounded-[8px] size-12">
+					<AvatarImage
+						src={server_img}
+						className="mx-auto my-auto object-cover"
 					/>
-				</AvatarFallback>
-			</Avatar>
+					<AvatarFallback className="bg-discord-blue rounded-[8px]">
+						<img
+							src="/icons/discord.svg"
+							className="w-[35px] h-[35px] mx-auto my-auto"
+						/>
+					</AvatarFallback>
+				</Avatar>
 
-			<CardContent className="grid gap-2 text-white/[60%] p-0">
-				<CardTitle className="text-white">{name}</CardTitle>
-				<div className="flex items-center gap-3 text-sm">
-					<div className="flex items-center gap-2">
-						<div className="bg-emerald size-3 rounded-full" />
-						{formatNumber(online)} online
+				<CardContent className="grid gap-2 text-white/[60%] p-0">
+					<CardTitle className="text-white">{name}</CardTitle>
+					<div className="flex items-center gap-3 text-sm">
+						<div className="flex items-center gap-2">
+							<div className="bg-emerald size-3 rounded-full" />
+							{formatNumber(online)} online
+						</div>
+						<div className="flex items-center gap-2">
+							<div className="bg-[#898989] size-3 rounded-full" />
+							{formatNumber(members)} members
+						</div>
 					</div>
-					<div className="flex items-center gap-2">
-						<div className="bg-[#898989] size-3 rounded-full" />
-						{formatNumber(members)} members
-					</div>
-				</div>
-				<p className="text-sm">Last seen: {lastSeen}</p>
-			</CardContent>
-		</Card>
+					<p className="text-sm">Last seen: {lastSeen}</p>
+				</CardContent>
+			</Card>
+		</Link>
 	);
 };
 
