f99d0e9be6b7123a9f6a1ea790fb9c852b88253b
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-02 15:37:40 +0100

    - lint fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,39 @@ import Help from "./pages/Help";
 import Inbox from "./pages/Inbox";
 import Servers from "./pages/Servers";
 import Profile from "./pages/Profile";
-import Sandbox from "./sandboxes/Sandbox";
 
 function App() {
 	return (
-		// <div className="flex relative w-screen h-screen overflow-hidden bg-charcoal">
-		// 	<Link
-		// 		to={"help"}
-		// 		className="absolute z-[100] bottom-2 right-2 rounded-full size-8 bg-onyx flex justify-center items-center text-white"
-		// 	>
-		// 		<QuestionMarkIcon />
-		// 	</Link>
-		// 	<CollapsibleSidebar />
+		<div className="flex relative w-screen h-screen overflow-hidden bg-charcoal">
+			<Link
+				to={"help"}
+				className="absolute z-[100] bottom-2 right-2 rounded-full size-8 bg-onyx flex justify-center items-center text-white"
+			>
+				<QuestionMarkIcon />
+			</Link>
+			<CollapsibleSidebar />
 
-		// 	<Routes>
-		// 		<Route element={<Main />}>
-		// 			<Route index element={<Navigate to="@me" replace />} />
+			<Routes>
+				<Route element={<Main />}>
+					<Route index element={<Navigate to="@me" replace />} />
 
-		// 			<Route path="@me">
-		// 				<Route index element={<Wumpus />} />
-		// 				<Route path="dm/:id" element={<MessagesLayout />} />
-		// 			</Route>
+					<Route path="@me">
+						<Route index element={<Wumpus />} />
+						<Route path="dm/:id" element={<MessagesLayout />} />
+					</Route>
 
-		// 			<Route path="@server/:id">
-		// 				<Route index element={<Wumpus />} />
-		// 				<Route path="@channel/:id" element={<ChannelsLayout />} />
-		// 			</Route>
-		// 		</Route>
+					<Route path="@server/:id">
+						<Route index element={<Wumpus />} />
+						<Route path="@channel/:id" element={<ChannelsLayout />} />
+					</Route>
+				</Route>
 
-		// 		<Route path="/servers" element={<Servers />} />
-		// 		<Route path="/inbox" element={<Inbox />} />
-		// 		<Route path="/help" element={<Help />} />
-		// 		<Route path="/profile/:id" element={<Profile />} />
-		// 	</Routes>
-		// </div>
-		<Sandbox />
+				<Route path="/servers" element={<Servers />} />
+				<Route path="/inbox" element={<Inbox />} />
+				<Route path="/help" element={<Help />} />
+				<Route path="/profile/:id" element={<Profile />} />
+			</Routes>
+		</div>
 	);
 }
 
