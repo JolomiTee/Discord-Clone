a4740d009f2291ad92f2176faf810a523c9cc749
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-12 22:26:02 +0100

    - created a placeholder profile page

---
+++ src/components/profile/RediscordPro.tsx
@@ -0,0 +1,5 @@
+const RediscordPro = () => {
+	return <div>RediscordPro</div>;
+};
+
+export default RediscordPro;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
+import "./assets/backgrounds.css";
 import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
 createRoot(document.getElementById("root")!).render(
