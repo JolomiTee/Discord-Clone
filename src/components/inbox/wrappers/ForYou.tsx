cae5c145b7e58f53f4f73393aad57a188ca46794
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-13 20:20:34 +0100

    - created wrapper components for inbox tabs -  moved Empty inbox ui to common - created basic inbox layout

---
+++ src/components/inbox/wrappers/ForYou.tsx
@@ -0,0 +1,25 @@
+import { for_you } from "@/data/notifications";
+import { AlertForYou } from "../for_you/Alert";
+import AccountSteps from "../for_you/AccountSteps";
+import RediscordPro from "../for_you/RediscordPro";
+import EmptyState from "@/components/common/EmptyNotificationState";
+
+const ForYou = () => {
+	return (
+		<div className="p-3 flex w-full gap-5">
+			<div className="w-full">
+				{for_you ? (
+					<AlertForYou />
+				) : (
+					<EmptyState text="You're all caught up!" />
+				)}
+			</div>
+			<div className="w-[50%] flex flex-col gap-10">
+				<AccountSteps />
+				<RediscordPro />
+			</div>
+		</div>
+	);
+};
+
+export default ForYou;
--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -1,9 +1,11 @@
-import AccountSteps from "@/components/inbox/for_you/AccountSteps";
-import RediscordPro from "@/components/inbox/for_you/RediscordPro";
+import ForYou from "@/components/inbox/wrappers/ForYou";
+import Mentions from "@/components/inbox/wrappers/Mentions";
+import Unread from "@/components/inbox/wrappers/Unread";
 import { Button } from "@/components/ui/button";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { CheckCheck } from "lucide-react";
+import { useState } from "react";
 const Inbox = () => {
 	return (
 		<Tabs defaultValue="for_you" className="w-full bg-onyx text-white">
@@ -19,39 +21,35 @@ const Inbox = () => {
 				</TabsTrigger>
 			</TabsList>
 
-			<div className="flex items-center justify-start gap-2 p-3 mb-3">
-				<Button className="rounded-[12px] bg-charcoal size-10 border border-solid border-[#FFFFFF0F] text-[#FFFFFF99]">
-					<CheckCheck />
-				</Button>
-			</div>
-
-			<TabsContent value="for_you" className="p-3 flex w-full gap-5">
-				<div className="w-full">
-					{}
-					<EmptyState text="You're all caught up!" />
+			<TabsContent value="for_you">
+				<div className="flex items-center justify-start gap-2 p-3 mb-3 text-[20px] font-bold">
+					For You
+					<Button className="rounded-[12px] bg-charcoal size-10 border border-solid border-[#FFFFFF0F] text-[#FFFFFF99]">
+						<CheckCheck />
+					</Button>
 				</div>
-				<div className="w-[50%] flex flex-col gap-10">
-					<AccountSteps />
-					<RediscordPro />
+				<ForYou />
+			</TabsContent>
+			<TabsContent value="unreads">
+				<div className="flex items-center justify-start gap-2 p-3 mb-3 text-[20px] font-bold">
+					Unreads
+					<Button className="rounded-[12px] bg-charcoal size-10 border border-solid border-[#FFFFFF0F] text-[#FFFFFF99]">
+						<CheckCheck />
+					</Button>
 				</div>
+				<Unread />
 			</TabsContent>
-			<TabsContent value="unreads" className=" p-3">
-				<EmptyState text="You've seen it all! No unreads messages" />
-			</TabsContent>
-			<TabsContent value="mentions" className=" p-3">
-				<EmptyState text="All quiet on the mentions front!" />
+			<TabsContent value="mentions">
+				<div className="flex items-center justify-start gap-2 p-3 mb-3 text-[20px] font-bold">
+					Mentions
+					<Button className="rounded-[12px] bg-charcoal size-10 border border-solid border-[#FFFFFF0F] text-[#FFFFFF99]">
+						<CheckCheck />
+					</Button>
+				</div>
+				<Mentions />
 			</TabsContent>
 		</Tabs>
 	);
 };
 
 export default Inbox;
-
-const EmptyState = ({ text }: { text: string }) => {
-	return (
-		<div className="w-full flex flex-col justify-center items-center gap-5">
-			<img src="/no-inbox.svg" className="w-1/2 md:w-1/3" />
-			<p>{text}</p>
-		</div>
-	);
-};
