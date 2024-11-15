387e557644a9e49b16519e060991517c2a1e7437
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-16 00:25:17 +0100

    - reversible tray icons modification

--- src/components/server/SidebarServerIcon.tsx
+++ src/components/server/SidebarServerIcon.tsx
@@ -59,7 +59,7 @@ const SidebarServerIcon = ({
 			className="group/item group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center "
 		>
 			<div
-				className={`absolute top-1/2 -translate-y-1/2 -left-[1px] w-1.5 md:w-2 rounded-full transition-all duration-100 ${
+				className={`absolute top-1/2 -translate-y-1/2 -left-0.5 w-1.5 md:w-2 rounded-full transition-all duration-100 ${
 					selectedServer === i
 						? "h-10 bg-discord-blue"
 						: hasNotification
@@ -74,7 +74,7 @@ const SidebarServerIcon = ({
 			<Link to={`@server/${String(i)}`} onClick={handleClick}>
 				<SidebarMenuButton
 					tooltip={title}
-					className="gap-3 text-base h-fit group-data-[collapsible=icon]:[&>span:last-child]:hidden p-0 ps-2 md:ps-3"
+					className="gap-3 text-base h-fit group-data-[collapsible=icon]:[&>span:last-child]:hidden p-0 ps-2"
 				>
 					<Avatar
 						className={`size-[45px] md:size-[50px] ${
