ce203eef789cb4dd27de4f7e27e9d456ef5a059b
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-22 02:00:13 +0100

    - adjusted route layouts - moved Hmenu out of root layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,25 +1,18 @@
-import HMenu from "@/components/common/HMenu";
 import { LSidebar, RSidebar } from "@/components/sidebar/Sidebars";
 import { SidebarInset } from "@/components/ui/sidebar";
-import { Outlet, useLocation, useParams } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const Main = () => {
-	const location = useLocation();
-	const { dmId } = useParams();
 	return (
 		<>
 			<LSidebar />
 
 			<SidebarInset className="w-full">
-				{(location.pathname.includes("@me/dm") ||
-					location.pathname.includes("/@channel")) && (
-					<HMenu dmId={dmId} />
-				)}
-
 				<div className="flex h-full overflow-hidden bg-charcoal">
 					<Outlet />
 				</div>
 			</SidebarInset>
+
 			<RSidebar />
 		</>
 	);
--- src/layouts/MainContainer.tsx
+++ src/layouts/MainContainer.tsx
@@ -4,7 +4,7 @@ interface Props {
 }
 const MainContainer = ({ children }: Props) => {
 	return (
-		<section className="p-3 md:p-4 lg:p-5 relative flex flex-col justify-between w-full bg-charcoal flex-1 overflow-auto scrollbar-hidden rounded-[15px]">
+		<section className="relative flex flex-col justify-between w-full bg-charcoal flex-1 overflow-auto scrollbar-hidden">
 			{children}
 		</section>
 	);
