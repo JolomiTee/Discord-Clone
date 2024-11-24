2a9b59b4e9e36d7fdbfc6b5ac016aa2cd548aeae
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-11-24 23:04:14 +0100

    - persisting with session storage to prevent LS new session bug

--- src/hooks/use-persist-app-state.ts
+++ src/hooks/use-persist-app-state.ts
@@ -2,12 +2,26 @@ import { AppPersistableStates } from "@/types";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+// Custom storage implementation for sessionStorage
+const sessionStorageProvider = {
+	getItem: (name: string) => {
+		const storedValue = sessionStorage.getItem(name);
+		return storedValue ? JSON.parse(storedValue) : undefined;
+	},
+	setItem: (name: string, value: any) => {
+		sessionStorage.setItem(name, JSON.stringify(value));
+	},
+	removeItem: (name: string) => {
+		sessionStorage.removeItem(name);
+	},
+};
+
 export const usePersistAppState = create<AppPersistableStates>()(
 	persist(
 		(set) => ({
 			selectedTab: "messages",
 			selectedServer: null,
-			l_sidebar_display_context: null,
+			l_sidebar_display_context: "messages", // Default to "messages"
 
 			switchLeftSidebarContext: (newAppState) => {
 				set({ l_sidebar_display_context: newAppState });
@@ -25,7 +39,8 @@ export const usePersistAppState = create<AppPersistableStates>()(
 			},
 		}),
 		{
-			name: "app-persisted-states", // Key name in localStorage
+			name: "app-persisted-states", // Key name in sessionStorage
+			storage: sessionStorageProvider, // Use sessionStorage
 		}
 	)
 );
