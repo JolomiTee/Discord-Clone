13701a489cc2acb20b7cea46fb4c439d15230c75
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-01-03 09:39:22 +0100

    Add FriendAction component for managing friend actions in FriendCard

---
+++ src/components/dialogs/FriendAction.tsx
@@ -0,0 +1,105 @@
+import {
+	AlertDialog,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useClerkRequest } from "@/hooks/use-query";
+import { Loader, ShieldBan, UserMinus2 } from "lucide-react";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
+import { useState } from "react";
+
+interface Props {
+	trigger: string;
+	friendId: string;
+	username: string;
+}
+const FriendAction = ({ trigger, friendId, username }: Props) => {
+	const { mutate, isLoading } = useClerkRequest("POST", ["added-friends"]);
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+	const handleAction = () => {
+		mutate(
+			{
+				url: `friend/${
+					trigger === "remove" ? "unfriend" : "block"
+				}/${friendId}`,
+			},
+			{
+				onSuccess: () => {
+					toast(`${username.trim()} is no longer a friend`);
+					setIsDialogOpen(false);
+				},
+				onError: (error) => {
+					toast.error(
+						`Could not unfriend ${username.trim()}, error: ${error}`
+					);
+					setIsDialogOpen(false);
+				},
+			}
+		);
+	};
+	return (
+		<AlertDialog
+			open={isDialogOpen || isLoading}
+			onOpenChange={setIsDialogOpen}
+		>
+			<AlertDialogTrigger className="hover:bg-discord-blue relative flex cursor-default select-none items-center gap-2 rounded w-full px-2 py-1.5 text-sm outline-none transition-colors focus:bg-discord-blue/70 focus:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-5 [&_svg]:shrink-0 dark:focus:bg-zinc-800 dark:focus:text-zinc-50">
+				{trigger === "remove" ? (
+					<>
+						<UserMinus2 /> Unfriend
+					</>
+				) : (
+					<>
+						<ShieldBan />
+						Block
+					</>
+				)}
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle className="truncate">
+						{trigger === "remove"
+							? `Really want to let a friend go?`
+							: `Block ${username}`}
+					</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone, however you can add them back by
+						searching for&nbsp;
+						<b className="text-black">{username.trim()}</b>
+						&nbsp;again
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel className="rounded-sm">
+						Keep 'em
+					</AlertDialogCancel>
+					<Button onClick={handleAction} disabled={isLoading}>
+						{trigger === "remove" ? (
+							isLoading ? (
+								<>
+									<Loader className="animate-spin w-2" /> Removing...
+								</>
+							) : (
+								"Unfriend"
+							)
+						) : isLoading ? (
+							<>
+								<Loader className="animate-spin w-2" /> Blocking...
+							</>
+						) : (
+							"Block"
+						)}
+					</Button>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+};
+
+export default FriendAction;
